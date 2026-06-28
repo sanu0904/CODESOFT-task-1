33753aea37b279d4627e1c25990576f16d58a3d7
sanu0904 <Sushantsrivastava0904@gmail.com>
2026-06-28 12:30:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 #chatbot-ALEX
 
- 
 print("Alex: Hello! I am Alex, your chatbot.")
 print("Alex: Type 'bye alex' if you want to end the chat.\n")
 
@@ -8,23 +7,23 @@ while True:
 
     user_input = input("SUSHANT: ").lower()
 
-    if user_message == "hello alex" or user_message == "hi alex":
+    if user_input == "hello alex" or user_input == "hi alex":
 
         print("Alex: Hi! Nice to meet you.")
 
-    elif "what is your name" in user_message:
+    elif "what is your name" in user_input:
         print(" My name is Alex.")
 
-    elif "how are you" in user_message:
+    elif "how are you" in user_input:
         print("Alex: I am fine. Thanks for asking!")
 
-    elif "who made you" in user_message:
+    elif "who made you" in user_input:
         print("Alex: my devloper is sushant sir")
 
-    elif "what is python" in user_message:
+    elif "what is python" in user_input:
         print("Alex: Python is an easy and powerful programming language.")
 
-    elif user_message == "bye alex":
+    elif user_input == "bye alex":
         print("Alex: Bye mate! Have a wonderful day.")
         break
 
